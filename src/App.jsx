488e3b92438d9c59a67752fe10f8e9ae6fa2ebb7
Spinkll <95828180+Spinkll@users.noreply.github.com>
2024-06-05 13:20:36 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import { useEffect, useState } from "react";
+
+function App() {
+  const getInitialFeedback = () => {
+    const savedFeedback = window.localStorage.getItem("saved-feedback");
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    } else {
+      return {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+    }
+  };
+
+  const [feedback, setFeedback] = useState(getInitialFeedback);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,5 @@
+const Notification = () => {
+  return <h2>No feedback yet</h2>;
+};
+
+export default Notification;
